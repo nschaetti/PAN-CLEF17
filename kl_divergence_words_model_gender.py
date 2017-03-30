bf291d1f9c72d541e8f99588def27554a8f685fe
Nils Schaetti <n.schaetti@gmail.com>
2017-03-30 14:49:17 +0200

    KL divergence

---
+++ kl_divergence_words_model_gender.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# File : create_dataset.py
+# Description : Create the data set for PAN17 with pySpeeches.
+# Date : 09 March 2017
+
+# Import packages
+import argparse
+import numpy as np
+import math
+from language_models.PAN17KLDivergenceModel import PAN17KLDivergenceModel
+from tools.PAN17Metrics import PAN17Metrics
+import cPickle as pickle
+
+# ALPHABETS
+ENGLISH_PUNCTUATIONS = u"?.!,;:#$§"
+SPANISH_PUNCTUATIONS = u"?¿.!¡,;:#$§"
+PORTUGUESE_PUNCTUATIONS = u"?.!,;:#$§"
+ARABIC_PUNCTUATIONS = u":?؟‎.!,;،؍"
+
+
+###########################
+# Start here
+###########################
+if __name__ == "__main__":
+
+    # Argument parser
+    parser = argparse.ArgumentParser(description="PAN17 language model for gender classification")
+
+    # Argument
+    parser.add_argument("--file", type=str, help="Input data set Pickle file", default="pan17clef.p")
+    parser.add_argument("--output", type=str, help="Output model Pickle file", default="output.p")
+    args = parser.parse_args()
+
+    # Load data set
+    with open(args.file, 'r') as f:
+        # Load
+        data_set = pickle.load(f)
+
+        # Sample size
+        n_samples = len(data_set['authors'])
+        fold_size = int(math.ceil(n_samples/10.0))
+
+        # Probabilistic model
+        language_model = PAN17KLDivergenceModel(classes=data_set['genders'], upper=False)
+
+        # K-10 fold
+        author_sets = np.array(data_set['authors'])
+        author_sets.shape = (10, fold_size)
+
+        # For each fold
+        error_rate_average = np.array([])
+        for i in range(10):
+            print("Error rate evaluation for K fold %d" % i)
+
+            # Initialize model
+            for doc in data_set['corpus'].get_documents():
+                for token in doc.get_tokens():
+                    language_model.init_token_count(token)
+                # end for
+            # end for
+
+            # Select training and test sets
+            test = author_sets[i]
+            training = np.delete(author_sets, i, axis=0)
+            training.shape = (fold_size * 9)
+
+            # For each author in training
+            for author in training:
+                gender = author.get_property("gender")
+                # For each doc
+                for doc in author.get_documents():
+                    # For each tokens
+                    for token in doc.get_tokens():
+                        language_model.inc_word(gender, token, 1.0)
+                    # end for
+                # end for
+            # end for
+
+            # Finalize model
+            language_model.finalize_model()
+
+            # Create author profile
+            docs_token = []
+            truths = []
+            for author in test:
+                author_tokens = []
+                for doc in author.get_documents():
+                    author_tokens += doc.get_tokens()
+                # end for
+                docs_token += [author_tokens]
+                truths += [author.get_property("gender")]
+            # end for
+
+            # Assess model error rate
+            error_rate = PAN17Metrics.error_rate(language_model, docs_token, truths) * 100.0
+            print("Error rate : %f %%" % error_rate)
+            error_rate_average = np.append(error_rate_average, error_rate)
+        # end for
+        print("10K Fold error rate for is %f" % np.average(error_rate_average))
+    # end with
+
+    # Save model
+    with open(args.output, 'w') as f:
+        print("Saving output file %s" % args.output)
+        # Write object
+        pickle.dump(language_model, f)
+    # end with
+
+# end if
---
+++ language_models/PAN17KLDivergenceModel.py
@@ -0,0 +1,190 @@
+# -*- coding: utf-8 -*-
+#
+# File : core/downloader/PySpeechesConfig.py
+# Description : .
+# Date : 20th of February 2017
+#
+# This file is part of pySpeeches.  pySpeeches is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright Nils Schaetti, University of Neuchâtel <nils.schaetti@unine.ch>
+
+import numpy as np
+from decimal import *
+from tools.PAN17Classifier import PAN17Classifier
+import math
+
+
+class PAN17KLDivergenceModel(PAN17Classifier):
+
+    # Constructor
+    def __init__(self, classes=[], upper=False):
+        """
+
+        :param classes:
+        """
+        super(PAN17KLDivergenceModel, self).__init__()
+        self._classes_counts = dict()
+        self._classes_tokens_count = dict()
+        self._collection_counts = dict()
+        self._collection_tokens_count = 0
+        for c in classes:
+            self._classes_counts[c] = dict()
+            self._classes_tokens_count[c] = 0
+        # end for
+        self._finalized = False
+        self._upper = upper
+    # end __init__
+
+    # Increment word
+    def inc_word(self, c, key, n):
+        """
+
+        :param c:
+        :param key:
+        :param n:
+        :return:
+        """
+        if not self._upper:
+            key = key.lower()
+        # end if
+        self._classes_counts[c][key] += n
+        self._classes_tokens_count[c] += n
+        self._collection_counts[key] += n
+        self._collection_tokens_count += n
+    # end inc_word
+
+    # Initialize token count
+    def init_token_count(self, key):
+        """
+
+        :param key:
+        :return:
+        """
+        if not self._upper:
+            key = key.lower()
+        # end if
+        for c in self._classes_counts.keys():
+            self._classes_counts[c][key] = 0.0
+        # end for
+        self._collection_counts[key] = 0.0
+    # end init_token_count
+
+    # Finalize model
+    def finalize_model(self):
+        """
+
+        :return:
+        """
+        # For each registered words
+        for c in self._classes_counts.keys():
+            for token in self._classes_counts[c].keys():
+                self._classes_counts[c][token] /= self._classes_tokens_count[c]
+            # end for
+        # end for
+        # For each token
+        for token in self._collection_counts.keys():
+            self._collection_counts[token] /= self._collection_tokens_count
+        # end for
+        self._finalized = True
+    # end finalize_model
+
+    # Word probability
+    def word_probability(self, token):
+        """
+
+        :param token:
+        :return:
+        """
+        if not self._upper:
+            token = token.lower()
+        # end if
+        prob = dict()
+        # For each classes
+        for c in self._classes_counts.keys():
+            prob[c] = self._classes_counts[c][token] * 10.0
+        # end for
+        return prob
+    # end word_probability
+
+    # Compute the KL divergence between two distribution
+    def _compute_kl_divergence(self, p, q):
+        """
+
+        :param p:
+        :param q:
+        :return:
+        """
+        count = 0.0
+        # For each words in distribution p
+        for token in p.keys():
+            if not self._upper:
+                token = token.lower()
+            # end if
+            count += p[token] * math.log(p[token] / q[token])
+        # end for
+        return count
+    # end _compute_KL_divergence
+
+    # Evaluate unseen document
+    def classify(self, tokens):
+        """
+
+        :param tokens:
+        :return:
+        """
+        # Precision
+        getcontext().prec = 256
+
+        # Initialize prob
+        total_tokens = float(len(tokens))
+        doc_probs = dict()
+        for token in self._collection_counts.keys():
+            doc_probs[token] = Decimal(0.0)
+        # end for
+
+        # For each tokens
+        for token in tokens:
+            doc_probs[token] += 1.0
+        # end for
+        doc_probs /= total_tokens
+
+        # Compute KL divergence for each classes
+        kl_divs = dict()
+        for c in self._classes_counts.keys():
+            kl_divs[c] = self._compute_kl_divergence(doc_probs, self._collection_counts)
+        # end
+
+        mini = 10000000000.0
+        winner = ""
+        # Get minimum divergence
+        for c in self._classes_counts.keys():
+            if kl_divs[c] < mini:
+                mini = kl_divs[c]
+                winner = c
+            # end if
+        # end for
+        return winner
+    # end evaluate_doc
+
+    # Get class model
+    def get_class_model(self, c):
+        class_model = []
+        c_dict = self._classes_counts[c]
+        for token in sorted(c_dict, key=c_dict.get, reverse=True):
+            class_model += [(token, c_dict[token])]
+        # end for
+        return class_model
+    # end get_class_model
+
+# end PAN17ProbabilisticModel
