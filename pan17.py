8babea41048aa91211fc3839dc6df41f3750c3da
nschaetti <n.schaetti@gmail.com>
2017-04-03 13:56:12 +0200

    PAN17

--- pan17.py
+++ pan17.py
@@ -190,12 +190,13 @@ if __name__ == "__main__":
     for lang in ["en", "es", "pt", "ar"]:
 
         # Create config file
-        print("Creating configuration files...")
+        print("Creating configuration files for language %s..." % lang)
         generate_config_file(lang, args.input_dataset, "/home/schaetti17/config")
 
         # Generate cleaned data set
         if not args.no_update or not os.path.exists(os.path.join("/home/schaetti17/inputs", lang,
                                                                  "pan17" + lang + ".p")):
+            print("Generating data set for %s" % lang)
             generate_data_set(os.path.join("/home/schaetti17/config", lang + ".json"),
                               os.path.join("/home/schaetti17/inputs", lang, "pan17" + lang + ".p"))
         # end if
